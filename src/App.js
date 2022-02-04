06cb7a57af2c99ddb87ad1bf28f874e3d7d529ed
l-Bohdan-l <bpapushoj@gmail.com>
2022-02-05 00:17:00 +0200

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import React from 'react';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ContactForm } from './components/ContactForm/ContactForm';
 import { ContactsList } from './components/ContactList/ContactList';
 import { Container } from './components/Container/Container';
 import { Filter } from './components/Filter/Filter';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { LoginForm } from './components/LoginForm/LoginForm';
+import { RegisterForm } from './components/RegisterForm/RegisterForm';
 
 import {
   addContact,
@@ -13,62 +17,74 @@ import {
   deleteContactsOps,
   filterContact,
 } from './redux/contacts/actions';
+import { Contacts } from './components/Contacts/Contacts';
 
 function App() {
-  const contacts = useSelector(state => state.phonebookReducers.contacts);
-  const filter = useSelector(state => state.phonebookReducers.filter);
-  const dispatch = useDispatch();
+  // const contacts = useSelector(state => state.phonebookReducers.contacts);
+  // const filter = useSelector(state => state.phonebookReducers.filter);
+  // const dispatch = useDispatch();
 
-  const createContact = newContact => {
-    const dublicateContact = contacts.some(checkedContact => {
-      return (
-        checkedContact.name.toLowerCase() === newContact.name.toLowerCase()
-      );
-    });
+  // const createContact = newContact => {
+  //   const dublicateContact = contacts.some(checkedContact => {
+  //     return (
+  //       checkedContact.name.toLowerCase() === newContact.name.toLowerCase()
+  //     );
+  //   });
 
-    if (dublicateContact) {
-      alert(`${newContact.name} is already in contacts`);
-      return;
-    }
-    dispatch(addContact(newContact));
-  };
+  //   if (dublicateContact) {
+  //     alert(`${newContact.name} is already in contacts`);
+  //     return;
+  //   }
+  //   dispatch(addContact(newContact));
+  // };
 
-  const handleDelete = contactId => {
-    dispatch(deleteContactsOps(contactId));
-  };
+  // const handleDelete = contactId => {
+  //   dispatch(deleteContactsOps(contactId));
+  // };
 
-  const changeFilter = e => {
-    const { value } = e.currentTarget;
-    dispatch(filterContact(value));
-  };
+  // const changeFilter = e => {
+  //   const { value } = e.currentTarget;
+  //   dispatch(filterContact(value));
+  // };
 
-  useEffect(() => {
-    dispatch(fetchContactsList());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContactsList());
+  // }, [dispatch]);
 
-  const getFilteredContacts = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()),
-    );
-  };
+  // const getFilteredContacts = () => {
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase()),
+  //   );
+  // };
 
-  const filteredContacts = getFilteredContacts();
+  // const filteredContacts = getFilteredContacts();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Container title="Phonebook">
-          <ContactForm createContact={createContact} />
-        </Container>
-        <Container title="Contacts">
-          <Filter value={filter} onChange={changeFilter} />
-          <ContactsList
-            contacts={filteredContacts}
-            onDelete={handleDelete}
-          ></ContactsList>
-        </Container>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <section className="App-header">
+          <Suspense fallback={<h2>Loading ...</h2>}>
+            <Routes>
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="*" element={<LoginForm />} />
+            </Routes>
+          </Suspense>
+
+          {/* <Container title="Phonebook">
+            <ContactForm createContact={createContact} />
+          </Container>
+          <Container title="Contacts">
+            <Filter value={filter} onChange={changeFilter} />
+            <ContactsList
+              contacts={filteredContacts}
+              onDelete={handleDelete}
+            ></ContactsList>
+          </Container> */}
+        </section>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,69 @@
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactsList } from '../ContactList/ContactList';
+import { Container } from '../Container/Container';
+import { Filter } from '../Filter/Filter';
+import {
+  addContact,
+  fetchContactsList,
+  deleteContactsOps,
+  filterContact,
+} from '../../redux/contacts/actions';
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export function Contacts() {
+  const contacts = useSelector(state => state.phonebookReducers.contacts);
+  const filter = useSelector(state => state.phonebookReducers.filter);
+  const dispatch = useDispatch();
+
+  const createContact = newContact => {
+    const dublicateContact = contacts.some(checkedContact => {
+      return (
+        checkedContact.name.toLowerCase() === newContact.name.toLowerCase()
+      );
+    });
+
+    if (dublicateContact) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    }
+    dispatch(addContact(newContact));
+  };
+
+  const handleDelete = contactId => {
+    dispatch(deleteContactsOps(contactId));
+  };
+
+  const changeFilter = e => {
+    const { value } = e.currentTarget;
+    dispatch(filterContact(value));
+  };
+
+  useEffect(() => {
+    dispatch(fetchContactsList());
+  }, [dispatch]);
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
+  return (
+    <>
+      <Container title="Phonebook">
+        <ContactForm createContact={createContact} />
+      </Container>
+      <Container title="Contacts">
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactsList
+          contacts={filteredContacts}
+          onDelete={handleDelete}
+        ></ContactsList>
+      </Container>
+    </>
+  );
+}
